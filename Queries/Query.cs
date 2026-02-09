91e5c3d87914d7f23849746751fc254a45eea70d
nikitaSkieller <101208939+nikitaSkieller@users.noreply.github.com>
2026-02-09 12:50:30 +0100

    Added queries for detailed entries

--- Queries/Query.cs
+++ Queries/Query.cs
@@ -11,8 +11,14 @@ public class Query
         db.Companies.Include(e => e.Projects).FirstOrDefaultAsync(e => e.Id == id);
 
     public IQueryable<Education> GetEducations([Service] CvDbContext db) => db.Educations;
+    public Task<Company?> GetEducationById(int id, [Service] CvDbContext db) =>
+    db.Companies.Include(e => e.Projects).FirstOrDefaultAsync(e => e.Id == id);
 
     public IQueryable<Project> GetProjects([Service] CvDbContext db) => db.Projects;
+    public Task<Company?> GetProjectById(int id, [Service] CvDbContext db) =>
+    db.Companies.Include(e => e.Projects).FirstOrDefaultAsync(e => e.Id == id);
 
     public IQueryable<Skill> GetSkills([Service] CvDbContext db) => db.Skills;
+    public Task<Company?> GetSkillById(int id, [Service] CvDbContext db) =>
+    db.Companies.Include(e => e.Projects).FirstOrDefaultAsync(e => e.Id == id);
 }
