20aa8ae98e14431652f5f2c6c0a9f6eb22276b4e
nikitaSkieller <101208939+nikitaSkieller@users.noreply.github.com>
2026-02-09 13:16:16 +0100

    Added UsePagingm UseFiltering and UseSorting

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Mindworking_Curriculum_Vitae.Data;
 using Mindworking_Curriculum_Vitae.Models;
+using HotChocolate.Data;
 
 public class Query
 {
-    // Companies
+    [UsePaging]
+    [UseFiltering]
+    [UseSorting]
     public IQueryable<Company> GetCompanies([Service] CvDbContext db) =>
         db.Companies.Include(e => e.Projects);
 
@@ -12,6 +15,9 @@ public class Query
         db.Companies.Include(e => e.Projects).FirstOrDefaultAsync(e => e.Id == id);
 
     // Educations
+    [UsePaging]
+    [UseFiltering]
+    [UseSorting]
     public IQueryable<Education> GetEducations([Service] CvDbContext db) =>
         db.Educations;
 
@@ -19,6 +25,9 @@ public class Query
         db.Educations.FirstOrDefaultAsync(e => e.Id == id);
 
     // Projects
+    [UsePaging]
+    [UseFiltering]
+    [UseSorting]
     public IQueryable<Project> GetProjects([Service] CvDbContext db) =>
         db.Projects;
 
@@ -26,6 +35,9 @@ public class Query
         db.Projects.FirstOrDefaultAsync(e => e.Id == id);
 
     // Skills
+    [UsePaging]
+    [UseFiltering]
+    [UseSorting]
     public IQueryable<Skill> GetSkills([Service] CvDbContext db) =>
         db.Skills;
 
