43dcf4f9f063cdba8d99a21b168c4c26e2f66dd2
nikitaSkieller <101208939+nikitaSkieller@users.noreply.github.com>
2026-02-09 12:56:00 +0100

    Fixed wrong return types in Queries

---
+++ GraphQL/Query.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mindworking_Curriculum_Vitae.Data;
+using Mindworking_Curriculum_Vitae.Models;
+
+public class Query
+{
+    // Companies
+    public IQueryable<Company> GetCompanies([Service] CvDbContext db) =>
+        db.Companies.Include(e => e.Projects);
+
+    public Task<Company?> GetCompanyById(int id, [Service] CvDbContext db) =>
+        db.Companies.Include(e => e.Projects).FirstOrDefaultAsync(e => e.Id == id);
+
+    // Educations
+    public IQueryable<Education> GetEducations([Service] CvDbContext db) =>
+        db.Educations;
+
+    public Task<Education?> GetEducationById(int id, [Service] CvDbContext db) =>
+        db.Educations.FirstOrDefaultAsync(e => e.Id == id);
+
+    // Projects
+    public IQueryable<Project> GetProjects([Service] CvDbContext db) =>
+        db.Projects;
+
+    public Task<Project?> GetProjectById(int id, [Service] CvDbContext db) =>
+        db.Projects.FirstOrDefaultAsync(e => e.Id == id);
+
+    // Skills
+    public IQueryable<Skill> GetSkills([Service] CvDbContext db) =>
+        db.Skills;
+
+    public Task<Skill?> GetSkillById(int id, [Service] CvDbContext db) =>
+        db.Skills.FirstOrDefaultAsync(e => e.Id == id);
+}
