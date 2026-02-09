fa324f252888838dfbf1b13a7d6b4abe76391bff
nikitaSkieller <101208939+nikitaSkieller@users.noreply.github.com>
2026-02-09 11:59:07 +0100

    added bdContext

--- Data/DbContext.cs
+++ Data/DbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace Mindworking_Curriculum_Vitae.Data
+﻿using Mindworking_Curriculum_Vitae.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mindworking_Curriculum_Vitae.Data
 {
-    public class DbContext
+    public class CvDbContext : DbContext
     {
+        public CvDbContext(DbContextOptions<CvDbContext> options) : base(options) { }
+
+        public DbSet<Company> Companies => Set<Company>();
+        public DbSet<Project> Projects => Set<Project>();
+        public DbSet<Education> Educations => Set<Education>();
+        public DbSet<Skill> Skills => Set<Skill>();
     }
 }
